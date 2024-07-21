2eb7623e841ff3414ecf28b55b78760281ecb665
melissadeleonx <primavita19@gmail.com>
2024-07-21 16:48:15 +0200

    google tag added

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 export default function Document() {
   return (
@@ -7,18 +8,22 @@ export default function Document() {
         <link rel="icon" href="/favicon.png" />
 
         {/* Google Tag Manager */}
-        <script
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-76N2RZQFV6"
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id=' + i + dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GT-TNPQHJDH');
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-76N2RZQFV6');
             `,
           }}
         />
-        {/* End Google Tag Manager */}
 
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
@@ -30,21 +35,11 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&family=Roboto+Mono&display=swap"
           rel="stylesheet"
         />
-        <script src="/scripts/no-flash.js"></script>
+        <Script src="/scripts/no-flash.js" strategy="lazyOnload" />
       </Head>
       <body className="text-text bg-bg">
         <Main />
         <NextScript />
-        {/* Google Tag Manager (noscript) */}
-        <noscript>
-          <iframe
-            src={`https://www.googletagmanager.com/ns.html?id=GT-TNPQHJDH`}
-            height="0"
-            width="0"
-            style={{ display: 'none', visibility: 'hidden' }}
-          />
-        </noscript>
-        {/* End Google Tag Manager (noscript) */}
       </body>
     </Html>
   );
